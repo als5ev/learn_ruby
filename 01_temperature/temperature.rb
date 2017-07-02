1839501732e89cf14fc5c64f7245b7929817f462
als5ev <als5ev@virginia.edu>
2017-07-01 20:50:35 -0400

    added completed files

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,7 @@
-#write your code here
+ def ftoc(num)
+ 	(num - 32) * 5/9.0
+ end
+
+ def ctof(num)
+ 	(num) * 9/5.0 + 32
+ end
--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,43 @@
 #write your code here
+def echo(word)
+	return word
+end
+
+def shout(word)
+	return word.upcase
+end
+
+def repeat(word, repititions=2)
+	output = word.clone
+	for i in 0 ... repititions-1
+		output << " "
+		output << word
+	end
+	return output
+end
+
+def start_of_word(word, index=1)
+	return word[0, index]
+end
+
+def first_word(word)
+	  new_word = word.split(" ")[0]
+end
+
+def titleize(word)
+	little_words = ["and", "the", "over"]
+	words = word.split(" ")
+
+	if little_words.include? words[0]
+		words[0].capitalize!
+	end
+
+	for i in 0 ... words.size
+		if ! little_words.include? words[i]
+			words[i].capitalize!
+		end
+	end
+
+	words = words.join(" ")
+	return words
+end
