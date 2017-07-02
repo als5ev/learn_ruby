083f6af4c71a5fd9ffbf3bf3fffb9666d7d7b9f4
als5ev <als5ev@virginia.edu>
2017-07-01 20:06:54 -0400

    first two lessons completed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
 #write your code here
+def add(int1, int2)
+	return int1 + int2
+end
+
+def subtract(int1, int2)
+	return int1-int2
+end
+
+def sum(arr)
+	sum = 0
+	for index in 0 ... arr.size
+		sum += arr[index]
+	end
+	return sum
+end
